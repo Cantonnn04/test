b2ec4209807404068c21c4b9d94b14a7bb85e1c3
Cantonnn04 <antongrinev04@gmail.com>
2024-09-13 09:55:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
+  import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,6 +20,7 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const auth = getAuth(app);
 
 const submitButton = document.getElementById("submit");
 const signupButton = document.getElementById("sign-up");
